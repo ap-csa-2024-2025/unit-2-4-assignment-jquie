27920d9cec232f5b17e591a42e8c9710e0a78713
jquie <jacquelynphan03@gmail.com>
2024-10-07 09:59:45 -0700

    done!!

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ public class Main
     {
         String words = lastFirstN("cream", "butter", 3);
         System.out.println(words);
+
+        stringManip ("hello", "hello"); 
+
     }
     
     public static String lastFirstN(String s1, String s2, int n)
@@ -11,9 +14,19 @@ public class Main
         int lastFirst = s1.length() - n;
         String output = s1.substring(lastFirst);
 
-        int lastSecond = s2.length() - n;
         String output2 = s2.substring(0,n);
 
         return (output + output2);
+    }
+
+    public static void stringManip (final String s, final String p)
+    {
+        System.out.println(s.toUpperCase());
+        String firstLetter = p.substring(0,1);
+        firstLetter = firstLetter.toUpperCase();
+
+        String lower = p.toLowerCase();
+        String restOfp = lower.substring(1);
+        System.out.println(firstLetter + restOfp);
     }
 }
